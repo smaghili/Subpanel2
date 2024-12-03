2acc00a4690b306c152918a72398090306affa55
aghili <aghili@apk.apk>
2024-12-03 11:38:59 +0330

    fix error

--- check_configs.php
+++ check_configs.php
@@ -1,6 +1,8 @@
 <?php
-+ error_reporting(E_ALL);
-+ ini_set('display_errors', 1);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', '/var/log/php_errors.log');
 
 // افزایش timeout
 set_time_limit(300);
